483498e7c74aa1e7a1f80b34c1864ef02df9f913
saadelhamidi <saadavicii123@gmail.com>
2024-03-20 17:41:44 -0700

    x

--- 0x04-python-more_data_structures/change_permission.py
+++ 0x04-python-more_data_structures/change_permission.py
@@ -1,6 +1,16 @@
 import os
 
-file_path = "C:\Users\saadoun\.ssh\alx-higher_level_programming\0x04-python-more_data_structures"
-permissions = 0o755
+def change_permissions(file_path, permissions):
+    try:
+        os.chmod(file_path, permissions)
+        print(f"Permissions changed for file: {file_path}")
+    except OSError as e:
+        print(f"Error: {e}")
 
-os.chmod(file_path, permissions)
+if __name__ == "__main__":
+    # Specify the file path and permissions
+    file_path = "C:\Users\saadoun\.ssh\alx-higher_level_programming\0x04-python-more_data_structures"
+    permissions = 0o755  # Numeric mode for rwxr-xr-x
+
+    # Call the function to change permissions
+    change_permissions(file_path, permissions)
