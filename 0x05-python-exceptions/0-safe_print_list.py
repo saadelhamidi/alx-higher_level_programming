c0530c037a20a40d68d1842d83c7f6169b37a081
saadelhamidi <saadavicii123@gmail.com>
2024-04-02 13:15:17 -0700

    x

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -10,5 +10,3 @@ def safe_print_list(my_list=[], x=0):
                                                                     except IndexError:
                                                                                 print("")
                                                                                         return (i)
-                                                                                        print("")
-                                                                                            return (i)
