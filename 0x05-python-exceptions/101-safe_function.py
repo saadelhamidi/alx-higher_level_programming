cef19e7fbf0bb12a20b14216d67efd028a09f87b
saadelhamidi <saadavicii123@gmail.com>
2024-04-02 13:27:18 -0700

    x

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    import sys
+
+    try:
+        result = fct(*args)
+        return (result)
+    except sys.exc_info()[0]:
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+        return (None)
